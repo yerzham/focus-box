3706a5ac06fd92cea8ae7068759c74f82a7d4785
Yerzhan Zhamashev <yerzhanzhamash@gmail.com>
2020-12-12 00:53:32 +0200

    Update config.js

--- config/config.js
+++ config/config.js
@@ -3,13 +3,7 @@ let config = {};
 if (Deno.args.length > 0) {
     config.database = Deno.env.toObject().DATABASE_URL;
   } else {
-    config.database = {
-        hostname: "hattie.db.elephantsql.com",
-        database: "jhutrobf",
-        user: "jhutrobf",
-        password: "TNcOy1BPUwgMO_t63kx6B0RzhglBp7e8",
-        port: 5432
-    };
+    config.database = {};
   }
 
 export { config }; 
